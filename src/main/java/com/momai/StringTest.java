6af589a71bf1bd04fb1bc0a3a2e3f59aa0d0d94b
lianxinzhong <macmini4@XingGudeMac-mini-4.local>
2017-02-10 17:18:36 +0800

    modify files

---
+++ src/main/java/com/momai/StringTest.java
@@ -0,0 +1,17 @@
+package com.momai;
+
+/**
+ * $
+ *
+ * @author Lian
+ * @date 2017/2/8
+ * @since 1.0
+ */
+public class StringTest {
+
+	public static void main(String[] args) {
+		String tempDateYmd = "20170208";
+		String endDateYmd = "20170208";
+		System.out.println(tempDateYmd.compareTo(endDateYmd));
+	}
+}
